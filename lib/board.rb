85b545c5a316663d22415021158de936a395c5c0
Abhigyan <abhigyanmahanta001@gmail.com>
2020-10-03 12:37:24 +0530

    define display board and set of win conditions

--- lib/board.rb
+++ lib/board.rb
@@ -9,5 +9,26 @@ class Board
 			[' ', ' ', ' ']
 		]
 		@inputs = 0
-  end
+	end
+	
+	def display_board
+		puts "#{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]}"
+		puts "----------------|-----------------|----------------"
+		puts "#{@board[1][0]} | #{@board[1][1]} | #{@board[1][2]}"
+		puts "----------------|-----------------|----------------"
+    puts "#{@board[2][0]} | #{@board[2][1]} | #{@board[2][2]}"
+	end
+	
+	def check_win(input)
+    win_conditions = [
+      [@board[0][0], @board[0][1], @board[0][2]],
+      [@board[1][0], @board[1][1], @board[1][2]],
+      [@board[2][0], @board[2][1], @board[2][2]],
+      [@board[0][0], @board[1][0], @board[2][0]],
+      [@board[0][1], @board[1][1], @board[2][1]],
+      [@board[0][2], @board[1][2], @board[2][2]],
+      [@board[0][0], @board[1][1], @board[2][2]],
+      [@board[0][2], @board[1][1], @board[2][0]]
+		]
+	end
 end
